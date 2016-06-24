f92656eff1934e2c1591a96d6ef58160365d6227
SirFrancisBillard <sirfrancisbillard@gmail.com>
2016-06-24 11:53:45 -1000

    Added content
    Demon clicker is a thing

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var ScrW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+var ScrH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+
+var demonPoints = 0
+var mostDemonPoints = 666
+var press = function() {
+	demonPoints++;
+	document.getElementById('score').innerHTML = "Your score: " + demonPoints;
+	if (demonPoints > mostDemonPoints) {
+		document.getElementById('highscore').innerHTML = "New high score: " + demonPoints;
+	}
+}
